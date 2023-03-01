fc14fa7288af3d3110f7266f34a9bbbc799fa5e3
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-02-28 20:19:44 -0600

    updated python script

--- python_scripts/create_csvs.py
+++ python_scripts/create_csvs.py
@@ -1,41 +1,38 @@
+"""
 import pandas as pd
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
-import math
+import random
 
 def incrementDate(curDate: date) -> date:
     '''increments the date we are generating orders for by 1 day'''
     curDate = curDate + timedelta(days=1)
     return curDate
 
-def add_random_item_sold():
-  menu_Contents = []
-  with open("Menu.csv") as menu:
-    #Content_list is the list that contains the read lines.     
-    for line in menu:
-      menu_Contents.append(line)
-    
-    for line in menu_Contents:
-      menu_Contents[line].split(",")
-
-    
-
+def add_random_item_sold(item_sold, order_num, prices):
+  index = random.randint(0,25)
+  cost = float(prices[index][1:len(prices[index])])
+  item_sold.append({"ITEM_ID" : len(item_sold) - 1, "MENU_ITEM_ID" : index, "ORDER_ID" : order_num, "INVENTORY_ID" : "NULL", "ITEM_SOLD_QUANTITY" : 1}, ignore_index=True)
   return cost
 
-def add_order(orders, item_sold, date):
-  num_items = math.random.randint(1,10)
+def add_order(orders, item_sold, date, prices):
+  num_items = random.randint(1,10)
   cost = 0
-
+  order_num = len(orders)
   for i in range(num_items):
-    cost += add_random_item_sold(item_sold)
+    cost += add_random_item_sold(item_sold, order_num, prices)
 
-  orders.append({'ORDER_ID' : len(orders) - 1, 'DATE_ORDERED' : date, 'ORDER_COST' : cost })
+  orders.append({'ORDER_ID' : len(orders) - 1, 'DATE_ORDERED' : date, 'ORDER_COST' : cost }, ignore_index=True)
   return cost
 
 gameDay = False;
 orders = pd.DataFrame(columns = ["ORDER_ID", "DATE_ORDERED", "ORDER_COST"])
-item_sold = pd.dataFrame(columns = ["ITEM_ID", "MENU_ITEM_ID", "ORDER_ID", "INVENTORY_ID", "ITEM_SOLD_QUANTITY"])
+item_sold = pd.DataFrame(columns = ['ITEM_ID', 'MENU_ITEM_ID', 'ORDER_ID', 'INVENTORY_ID', 'ITEM_SOLD_QUANTITY'])
+
+Menu = pd.read_csv('database - Menu.csv')
+prices = Menu["NEMU_ITEM_COST (float)"].values.tolist()
+
 
 dateFormat = "%Y-%m-%d"
 startDateString = "2022-01-01"
@@ -52,7 +49,64 @@ for i in range(365):
     num_orders = 15000
 
   for j in range(num_orders):
-    total_cost += add_order(orders, item_sold, date)
+    total_cost += add_order(orders, item_sold, date, prices)
 print(total_cost)
 orders.to_csv('Orders.csv')
 item_sold.to_csv('ItemSold.csv')
+"""
+import pandas as pd
+from datetime import date, timedelta, datetime
+import random
+
+def increment_date(cur_date: date) -> date:
+    '''increments the date we are generating orders for by 1 day'''
+    return cur_date + timedelta(days=1)
+
+def add_random_item_sold(item_sold, order_num, prices):
+    index = random.randint(0, 6)
+    cost = round(float(prices[index][1:]), 2)
+    item = {"ITEM_ID": len(item_sold), "MENU_ITEM_ID": index, "ORDER_ID": order_num,
+            "INVENTORY_ID": "NULL", "ITEM_SOLD_QUANTITY": 1}
+    item_sold = item_sold.append(item, ignore_index=True)
+    return cost, item_sold
+
+def add_order(orders, item_sold, date_str, prices):
+    num_items = random.randint(1, 10)
+    cost = 0
+    order_num = len(orders)
+    for i in range(num_items):
+        item_cost, item_sold = add_random_item_sold(item_sold, order_num, prices)
+        cost += item_cost
+
+    order = {'ORDER_ID': order_num, 'DATE_ORDERED': date_str, 'ORDER_COST': cost}
+    orders = orders.append(order, ignore_index=True)
+    return cost, orders, item_sold
+
+menu = pd.read_csv('database - Menu.csv')
+prices = menu["NEMU_ITEM_COST (float)"].values.tolist()
+
+date_format = "%Y-%m-%d"
+start_date_string = "2022-02-27"
+start_date = datetime.strptime(start_date_string, date_format).date()
+curr_date = start_date
+
+orders = pd.DataFrame(columns=["ORDER_ID", "DATE_ORDERED", "ORDER_COST"])
+item_sold = pd.DataFrame(columns=['ITEM_ID', 'MENU_ITEM_ID', 'ORDER_ID', 'INVENTORY_ID', 'ITEM_SOLD_QUANTITY'])
+
+total_cost = 0
+for i in range(365):
+    curr_date = increment_date(curr_date)
+
+    if i % 21 == 0:
+        num_orders = 400
+    else:
+        num_orders = 100
+
+    for j in range(num_orders):
+        cost, orders, item_sold = add_order(orders, item_sold, str(curr_date), prices)
+        total_cost += cost
+    print(str(i) + " : " + str(round(total_cost,2)))
+orders.to_csv('Orders.csv', index=False)
+item_sold.to_csv('ItemSold.csv', index=False)
+
+print(round(total_cost, 2))
