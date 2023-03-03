240e92b34d3ca1a58b2f435939445fe188ce855b
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-03-03 10:26:39 -0600

    moved java files

---
+++ javaFX/manager.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+
+public static void main(String args[]) {
+    Connection conn = null;
+    String teamNumber = "team_42";
+    String dbName = "csce315331_" + teamNumber;
+    String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+    dbSetup myCredentials = new dbSetup(); 
+
+    //Connecting to the database
+    try {
+        conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+     } catch (Exception e) {
+        e.printStackTrace();
+        System.err.println(e.getClass().getName()+": "+e.getMessage());
+        System.exit(0);
+     }
+
+}
