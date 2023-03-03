ce1da546068d9d36f962fb93f61b401aa0134884
Ariela Mitrani <amitrani@tamu.edu>
2023-03-03 10:11:40 -0600

    JavaFX imported, employee GUI draft in.

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+ 
+public class Main extends Application { // could be different
+    public static void main(String[] args) { launch(args); }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	FXMLLoader loader = new FXMLLoader(
+    	        getClass().getResource("login.fxml")
+    	);
+    	//loader.setController(loader.getController());
+    	Parent root = (Parent) loader.load();
+    	
+        primaryStage.setTitle("315 Project 2");
+        primaryStage.setScene(new Scene(root, 750, 750));
+        primaryStage.show();
+    }
+    
+    
+    
+    
+}
---
+++ Makefile
@@ -0,0 +1,26 @@
+JFLAGS = -g
+JC = javac
+JVM = java
+.SUFFIXES: .java .class
+.java.class:
+	$(JC) $(JFLAGS) $*.java --module-path "javaFX/lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics *.java
+TERMINAL = Driver
+GUI = Main
+
+CLASSES = \
+	Controller.java \
+	Main.java
+
+
+default: classes
+
+classes: $(CLASSES:.java=.class)
+
+noGUI: classes
+	$(JVM) $(TERMINAL)
+
+GUI: classes
+	$(JVM) --module-path "javaFX/lib" --add-modules javafx.controls,javafx.fxml $(GUI) 
+
+clean:
+	$(RM) *.class
