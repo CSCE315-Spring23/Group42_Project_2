194f903c1bcba1e6cea677d61fbad538bcb44b3a
Bryan Yan <bryanyan.tx@gmail.com>
2023-03-20 15:39:26 -0500

    added documentation for Menu

--- src/Menu.java
+++ src/Menu.java
@@ -5,20 +5,38 @@ public class Menu {
     private SimpleObjectProperty<String> itemName;
     private SimpleObjectProperty<Double> itemCost;
 
+    /**
+     * Constructor for menu
+     * @param id menu ID
+     * @param name item name
+     * @param cost cost of item
+     */
     public Menu(final Long id, final String name, final Double cost){
         this.menuID = new SimpleObjectProperty<>(id);
         this.itemName = new SimpleObjectProperty<>(name);
         this.itemCost = new SimpleObjectProperty<>(cost);
     }
     
+    /**
+     * 
+     * @return menu ID
+     */
     public SimpleObjectProperty<Long> getMenuID(){
         return this.menuID;
     }
     
+    /**
+     * 
+     * @return item name
+     */
     public SimpleObjectProperty<String> getItemName(){
         return this.itemName;
     }
     
+    /**
+     * 
+     * @return item cost
+     */
     public SimpleObjectProperty<Double> getItemCost(){
         return this.itemCost;
     }
