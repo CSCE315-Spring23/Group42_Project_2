a4f9c8da767b09ebe57784454c8e2ee916b1c882
Bryan Yan <bryanyan.tx@gmail.com>
2023-03-20 15:48:19 -0500

    added documentation for SaleData

--- src/SaleData.java
+++ src/SaleData.java
@@ -5,20 +5,39 @@ public class SaleData {
     private SimpleObjectProperty<String> menuItemName;
     private SimpleObjectProperty<Long> totalQuantity;
 
+    /**
+     * Constructor for SaleData
+     * @param menuItemId
+     * @param menuItemName
+     * @param totalQuantity
+     */
     public SaleData(Long menuItemId, String menuItemName, Long totalQuantity) {
         this.menuItemId = new SimpleObjectProperty<>(menuItemId);
         this.menuItemName = new SimpleObjectProperty<>(menuItemName);
         this.totalQuantity = new SimpleObjectProperty<>(totalQuantity);
     }
 
+    /**
+     * 
+     * @return menu item ID
+     */
     public SimpleObjectProperty<Long> getMenuItemId() {
         return this.menuItemId;
     }
 
+    /**
+     * 
+     * @return menu item name
+     */
     public SimpleObjectProperty<String> getMenuItemName() {
         return this.menuItemName;
     }
 
+    /**
+     * 
+     * 
+     * @return total quantity of sales
+     */
     public SimpleObjectProperty<Long> getTotalQuantity() {
         return this.totalQuantity;
     }
