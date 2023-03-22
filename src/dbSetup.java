afd7129c781d17ea6491b8315ad97c60f14d26e0
Arjun Kurkal <arjunkurkal@gmail.com>
2023-03-21 23:04:47 -0500

    dbSetup comments

--- src/dbSetup.java
+++ src/dbSetup.java
@@ -1,5 +1,9 @@
+/**
+ * Creates class dbSetup, stores credentials for database login
+ */
 public class dbSetup {
-  // Database Username and Password
+  // Database Username
   public static final String user = "csce315331_team_42_master";
+  // Database Password
   public static final String pswd = "password";
 }
