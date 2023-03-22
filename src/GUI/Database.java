0962d5782fd6c65ea0270e2c34834e444f90c0bb
SrikarPotlapalli <srikarpotlapallis@gmail.com>
2023-03-22 15:13:15 -0500

    small bug fixes

--- src/GUI/Database.java
+++ src/GUI/Database.java
@@ -417,7 +417,8 @@ public class Database {
         ObservableList<ReportContent> rows = FXCollections.observableArrayList();
         try {
             // run query
-            ResultSet result = runCommand(String.format("SELECT * FROM zreportcontent WHERE report_id= '%d'", whichTwenty));
+            ResultSet result = runCommand("SELECT * FROM zreportcontent");
+            // ResultSet result = runCommand(String.format("SELECT * FROM zreportcontent WHERE report_id= '%d'", whichTwenty));
 
             // Get metadata which gets info about the types/properties of the columns in a
             // ResultSet
