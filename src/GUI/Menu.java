6cff4df9fdcaa01e6b9ccce1c7f2dba247d01a97
Daniela Santos <danis@DanielaLaptop>
2023-03-23 13:34:11 -0500

    finished documentation in most files

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -1,15 +1,30 @@
 import javafx.beans.property.SimpleObjectProperty;
 
+/**
+ * Represents an item on the restaurant's menu.
+ */
 public class Menu {
+    /** 
+     * The ID of the menu item. 
+     */
     private SimpleObjectProperty<Long> menuID;
+    
+    /** 
+     * The name of the menu item. 
+     */
     private SimpleObjectProperty<String> itemName;
+    
+    /** 
+     * The cost of the menu item. 
+     */
     private SimpleObjectProperty<Double> itemCost;
-
+    
     /**
-     * Constructor for menu
-     * @param id menu ID
-     * @param name item name
-     * @param cost cost of item
+     * Constructs a new Menu item.
+     * 
+     * @param id the menu item's ID
+     * @param name the name of the menu item
+     * @param cost the cost of the menu item
      */
     public Menu(final Long id, final String name, final Double cost){
         this.menuID = new SimpleObjectProperty<>(id);
@@ -18,27 +33,29 @@ public class Menu {
     }
     
     /**
+     * Returns the menu item's ID.
      * 
-     * @return menu ID
+     * @return the menu item's ID
      */
     public SimpleObjectProperty<Long> getMenuID(){
         return this.menuID;
     }
     
     /**
+     * Returns the name of the menu item.
      * 
-     * @return item name
+     * @return the name of the menu item
      */
     public SimpleObjectProperty<String> getItemName(){
         return this.itemName;
     }
     
     /**
+     * Returns the cost of the menu item.
      * 
-     * @return item cost
+     * @return the cost of the menu item
      */
     public SimpleObjectProperty<Double> getItemCost(){
         return this.itemCost;
     }
-    
 }
--- src/GUI/Recipe.java
+++ src/GUI/Recipe.java
@@ -1,66 +1,95 @@
 import javafx.beans.property.SimpleObjectProperty;
 
+/**
+ * Represents a recipe in the system.
+ */
 public class Recipe {
+    /**
+     * The ID of the recipe.
+     */
     private SimpleObjectProperty<Long> recipeID;
+    
+    /**
+     * The name of the recipe.
+     */
     private SimpleObjectProperty<String> recipeName;
+    
+    /**
+     * The ID of the inventory used in the recipe.
+     */
     private SimpleObjectProperty<Long> recipeInventoryID;
+    
+    /**
+     * The ID of the menu to which the recipe belongs.
+     */
     private SimpleObjectProperty<Long> recipeMenuID;
+    
+    /**
+     * The quantity of the recipe.
+     */
     private SimpleObjectProperty<Double> recipeQuantity;
 
 
+
     /**
-     * Recipe Constructor
-     * @param id recipe ID
-     * @param name recipe name
-     * @param iid inventory ID
-     * @param mid menu ID
-     * @param quantity recipe quantity
+     * Constructor for the Recipe class.
+     * 
+     * @param id       the recipe ID
+     * @param name     the recipe name
+     * @param iid      the inventory ID
+     * @param mid      the menu ID
+     * @param quantity the recipe quantity
      */
-    public Recipe(final Long id, final String name, final Long iid, final Long mid, final Double quantity){
+    public Recipe(final Long id, final String name, final Long iid, final Long mid, final Double quantity) {
         this.recipeID = new SimpleObjectProperty<>(id);
         this.recipeName = new SimpleObjectProperty<>(name);
         this.recipeInventoryID = new SimpleObjectProperty<>(iid);
         this.recipeMenuID = new SimpleObjectProperty<>(mid);
         this.recipeQuantity = new SimpleObjectProperty<>(quantity);
     }
-    
+
     /**
+     * Returns the recipe ID.
      * 
-     * @return recipe ID
+     * @return the recipe ID
      */
-    public SimpleObjectProperty<Long> getRecipeID(){
+    public SimpleObjectProperty<Long> getRecipeID() {
         return this.recipeID;
     }
-    
+
     /**
+     * Returns the recipe name.
      * 
-     * @return recipe name
+     * @return the recipe name
      */
-    public SimpleObjectProperty<String> getRecipeName(){
+    public SimpleObjectProperty<String> getRecipeName() {
         return this.recipeName;
     }
 
     /**
+     * Returns the inventory ID.
      * 
-     * @return recipe ID
+     * @return the inventory ID
      */
-    public SimpleObjectProperty<Long> getInventoryID(){
+    public SimpleObjectProperty<Long> getInventoryID() {
         return this.recipeInventoryID;
     }
 
     /**
+     * Returns the menu ID.
      * 
-     * @return menu ID
+     * @return the menu ID
      */
-    public SimpleObjectProperty<Long> getMenuID(){
+    public SimpleObjectProperty<Long> getMenuID() {
         return this.recipeMenuID;
     }
-    
+
     /**
+     * Returns the recipe quantity.
      * 
-     * @return recipe quantity
+     * @return the recipe quantity
      */
-    public SimpleObjectProperty<Double> getQuantity(){
+    public SimpleObjectProperty<Double> getQuantity() {
         return this.recipeQuantity;
     }
     
